614e42aca53da2f5a8a3675ea1519f638001f1f9
HanslettTheDev <hanslettthedev@gmail.com>
2020-05-16 22:46:40 +0100

    removed saved report from same location added button and m

--- report.py
+++ report.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reports.db'
 db = SQLAlchemy(app)
-
-# class MyDateTime(db.TypeDecorator):
-#     impl = db.DateTime
-    
-#     def process_bind_param(self, value, dialect):
-#         if type(value) is str:
-#             return datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
-#         return value
 
 class StoreReports(db.Model):
     # TODO: Create A database to store all the form
@@ -38,6 +30,35 @@ def report_home():
 
 @app.route('/reports', methods=['GET', 'POST'])
 def new_report():
+    return render_template('report.html')
+
+@app.route('/reports/delete/<int:id>')
+def delete(id):
+    reports = StoreReports.query.get_or_404(id) #check the id or return 404 error
+    db.session.delete(reports) #if found delete report
+    db.session.commit() #commit the change in the database to reflect changes in front end
+    return redirect('/Saved Reports') #when done redirect to same page
+
+@app.route('/reports/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    editreport = StoreReports.query.get_or_404(id)
+    if request.method == 'POST':
+        # create an object that holdes the previously submitted data based on the id
+        editreport.report_name = request.form['r-name']
+        editreport.report_date = request.form['r-date']
+        editreport.report_hrs = request.form['hrs']
+        editreport.report_vid = request.form['vid']
+        editreport.report_rv = request.form['rv']
+        editreport.report_bs = request.form['bs']
+        editreport.report_plc = request.form['plc']
+        editreport.report_min = request.form['min']
+        db.session.commit()
+        return redirect('/Saved Reports')
+    else:
+        return render_template('edit.html', reports=editreport)
+
+@app.route('/Saved Reports', methods=['GET', 'POST'])
+def SavedReports():
     if request.method == 'POST':
         # if the request mode is post get all data from form and store in a variable
         report_name = request.form['r-name']
@@ -51,36 +72,11 @@ def new_report():
         new_report = StoreReports(report_name=report_name, report_date=report_date, report_hrs=report_hrs, report_vid=report_vid, report_rv=report_rv, report_bs=report_bs, report_plc=report_plc, report_min=report_min)
         db.session.add(new_report)
         db.session.commit()
-        return redirect('/reports')
+        return redirect('/Saved Reports')
     else:
         all_reports = StoreReports.query.order_by(StoreReports.id).all()
-        return render_template('report.html', reports=all_reports)
+        return render_template('saved.html', reports=all_reports)
 
-@app.route('/reports/delete/<int:id>')
-def delete(id):
-    reports = StoreReports.query.get_or_404(id) #check the id or return 404 error
-    db.session.delete(reports) #if found delete report
-    db.session.commit() #commit the change in the database to reflect changes in front end
-    return redirect('/reports') #when done redirect to same page
-
-@app.route('/reports/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-
-    editreport = StoreReports.query.get_or_404(id)
-    if request.method == 'POST':
-        # create an object that holdes the previously submitted data based on the id
-        editreport.report_name = request.form['r-name']
-        editreport.report_date = request.form['r-date']
-        editreport.report_hrs = request.form['hrs']
-        editreport.report_vid = request.form['vid']
-        editreport.report_rv = request.form['rv']
-        editreport.report_bs = request.form['bs']
-        editreport.report_plc = request.form['plc']
-        editreport.report_min = request.form['min']
-        db.session.commit()
-        return redirect('/reports')
-    else:
-        return render_template('edit.html', reports=editreport)
 
 
 if __name__ == "__main__":
--- static/js/ReportApp.js
+++ static/js/ReportApp.js
@@ -1,8 +1,38 @@
 // Selectors
-// const saveClickAlert = document.getElementById('btn-alert');
-// const getMin = ge
+const formEvent = document.getElementById('btn-alert');
+const reportName = document.getElementById('r-name');
+const reportRv = document.getElementById('rv');
+const reportBs = document.getElementById('bs');
+const reportMin = document.getElementById('min');
+const text = document.getElementById('error-text');
+// const version = document.getElementById('version');
 
-// events
+// events and Callback event functions
+// function versionAlert() {
+//     alert('Whats New In v' + version.value + ' Field Service Report App');
+// }
+
+formEvent.addEventListener('click', function (event) {
+    let errorMessages = [];
+    if (reportName.value === '' || reportName.value == null) {
+        errorMessages.push('Hey, You Forgot To Create A Report Name');
+        text.className += "alert alert-danger";
+    }
+
+    if (reportMin.value > 60) {
+        alert(reportMin.value + ' Minutes Is higher Than 1 Hour! Just Add To Your Hours');
+    }
+
+    if (reportBs.value > reportRv.value) {
+        errorMessages.push('Sorry, You Cant Have ' + reportBs.value + ' Bible Studies And Just ' + reportRv.value + ' Return Visits');
+        text.className += "alert alert-danger";
+    }
+
+    if (errorMessages.length > 0) {
+        event.preventDefault();
+        text.innerHTML = errorMessages.join(', ');
+    }
+});
 
 
 // functions
